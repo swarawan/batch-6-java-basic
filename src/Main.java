4e3b1f3dedd219f87043734120ff88ba5d35387a
Rio Swarawan Putra <swarawan.rio@gmail.com>
2018-03-09 21:16:25 +0700

    [RIO] add if else

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Mobil avanza = new Mobil("Sumber Buku");
-        avanza.printNama("Annas");
-        avanza.print();
-        System.out.println("Jumlah Roda Baru = " + avanza.tambahRoda());
+        Parkir parkir = new Parkir();
+        parkir.parkirIfElse(6);
     }
 }
---
+++ src/Parkir.java
@@ -0,0 +1,28 @@
+/**
+ * Created by rioswarawan on 3/9/18.
+ */
+public class Parkir {
+
+    /**
+     * total roda <= 2 		-> lahan parkir sebelah kiri
+     * total roda > 2 <=4 	-> lahan parkir sebelah kanan
+     * total roda > 4		-> basement
+     */
+
+    public void parkirIfElse(int jumlahRoda) {
+        System.out.println("Jumlah Roda : " + jumlahRoda);
+        if (jumlahRoda <= 2) {
+            System.out.println("Kendaraan diparkir di sebelah kiri gedung");
+        } else if (jumlahRoda <= 4) {
+            System.out.println("Kendaraan diparkir di sebelah kanan gedung");
+        } else {
+            System.out.println("Kendaraan diparkir di basement");
+        }
+    }
+
+    public void parkirSwitchCase(int jumlahRoda) {
+        switch (jumlahRoda) {
+            case 2:
+        }
+    }
+}
