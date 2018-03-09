cf1cf3364db523cac90e4222854230348b718550
Rio Swarawan Putra <swarawan.rio@gmail.com>
2018-03-09 22:10:33 +0700

    [RIO] add loop functions

--- src/Parkir.java
+++ src/Parkir.java
@@ -9,36 +9,78 @@ public class Parkir {
      * total roda > 4		-> basement
      */
 
-    int jumlahRoda = 3;
+    public void parkirIfElse(int jumlahRoda) {
 
-    public void parkirIfElse() {
-
-        System.out.println("Jumlah Roda : " + jumlahRoda);
+        System.out.print("Kendaraan dengan jumlah roda " + jumlahRoda);
 
         if (jumlahRoda <= 2) {
-            System.out.println("Kendaraan diparkir di sebelah kiri gedung");
+            System.out.println(" diparkir di sebelah kiri gedung");
 
         } else if (jumlahRoda > 3 && jumlahRoda <= 4) {
-            System.out.println("Kendaraan diparkir di sebelah kanan gedung");
+            System.out.println(" diparkir di sebelah kanan gedung");
 
         } else {
-            System.out.println("Kendaraan diparkir di basement");
+            System.out.println(" diparkir di basement");
 
         }
     }
 
     public void parkirSwitchCase(int jumlahRoda) {
-        System.out.println("Jumlah Roda : " + jumlahRoda);
+        System.out.print("Kendaraan dengan jumlah roda " + jumlahRoda);
         switch (jumlahRoda) {
             case 2:
-                System.out.println("Kendaraan diparkir di sebelah kiri gedung");
+                System.out.println(" diparkir di sebelah kiri gedung");
                 break;
             case 4:
-                System.out.println("Kendaraan diparkir di sebelah kanan gedung");
+                System.out.println(" diparkir di sebelah kanan gedung");
                 break;
             default:
-                System.out.println("Kendaraan diparkir di basement");
+                System.out.println(" diparkir di basement");
                 break;
         }
+    }
+
+    public void parkir() {
+
+        // array
+        int[] kumpulanTotalRoda = {2, 5, 6, 9, 4, 2, 18, 19};
+
+        for (int index = 0; index < kumpulanTotalRoda.length; index++) {
+            int totalRoda = kumpulanTotalRoda[index];
+            parkirIfElse(totalRoda);
+        }
+    }
+
+    public void parkir4() {
+        int[] kumpulanTotalRoda = {2, 5, 6, 9, 4, 2, 18, 19};
+
+        for (int index : kumpulanTotalRoda) {
+            int totalRoda = kumpulanTotalRoda[index];
+            parkirIfElse(totalRoda);
+        }
+    }
+
+    public void parkir2() {
+        int[] kumpulanTotalRoda = {2, 5, 6, 9, 4, 2, 18, 19};
+
+        int index = 0;
+        while (index < kumpulanTotalRoda.length) {
+            int totalRoda = kumpulanTotalRoda[index];
+            parkirIfElse(totalRoda);
+
+            index++;
+        }
+    }
+
+    public void parkir3() {
+        int[] kumpulanTotalRoda = {2, 5, 6, 9, 4, 2, 18, 19};
+
+        int index = 0;
+        do {
+            int totalRoda = kumpulanTotalRoda[index];
+            parkirIfElse(totalRoda);
+
+            index++;
+        } while (index < kumpulanTotalRoda.length);
     }
 }
