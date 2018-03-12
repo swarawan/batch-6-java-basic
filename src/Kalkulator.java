e4fc7ff2c66deb9ebd77faaaa918bcadd0a35fc4
Rio Swarawan Putra <swarawan.rio@gmail.com>
2018-03-12 21:52:31 +0700

    [RIO] add kalkulator model

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
  */
 public class Kalkulator {
 
-    public Kalkulator() {
+    KalkulatorModel model = new KalkulatorModel();
+
+    public void printKalkulator() {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.print("Masukkan Nama : ");
-            int nilai1 = scanner.nextInt();
+            System.out.print("Masukkan Nilai 1 : ");
+            model.setNilai1(scanner.nextInt());
 
-            System.out.print("Masukkan NIM : ");
-            int nilai2 = scanner.nextInt();
+            System.out.print("Masukkan Nilai 2 : ");
+            model.setNilai2(scanner.nextInt());
 
             System.out.println("Pilih Operator");
             System.out.println("1. Tambah");
@@ -23,15 +25,24 @@ public class Kalkulator {
             System.out.print("Pilihan (1..4)? ");
             int operator = scanner.nextInt();
 
-            hitung(nilai1, nilai2, operator);
+            hitung(operator);
         } catch (Exception ex) {
-            System.out.print("Error: " + ex.getMessage());
+            errorHandling();
+        } finally {
+            printKalkulator();
         }
     }
 
-    private void hitung(int nilai1, int nilai2, int operator) {
+    private void errorHandling() {
+        System.out.println("Salah brooooooo.... integer harusnya");
+    }
 
+    private void hitung(int operator) {
+
+        int nilai1 = model.getNilai1();
+        int nilai2 = model.getNilai2();
         int hasil;
+        
         if (operator == 1) {
             hasil = nilai1 + nilai2;
             System.out.printf("%d + %d = %d", nilai1, nilai2, hasil);
---
+++ src/KalkulatorModel.java
@@ -0,0 +1,24 @@
+/**
+ * Created by rioswarawan on 3/12/18.
+ */
+public class KalkulatorModel {
+
+    private int nilai1;
+    private int nilai2;
+
+    public void setNilai1(int n1) {
+        nilai1 = n1;
+    }
+
+    public int getNilai1() {
+        return nilai1;
+    }
+
+    public void setNilai2(int nilai2) {
+        this.nilai2 = nilai2;
+    }
+
+    public int getNilai2() {
+        return nilai2;
+    }
+}
